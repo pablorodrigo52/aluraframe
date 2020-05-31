1e01f79edf275b8dc6f03a2cbbdc84a0eb7ea109
Pablo Rodrigo <pablo.santos@softgreen.com.br>
2020-05-31 16:28:24 -0300

    Work with arrow function

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -7,9 +7,9 @@ class NegociacaoController {
     constructor (){
         let $ = document.querySelector.bind(document); // cria um alias para a função document.querySelector('');
         /* Simulando a sintaxe do jquery */    
-        this._inputDate = $('#date').value;
-        this._inputValor = $('#valor').value;
-        this._inputQuantidade = $('#quantidade').value;
+        this._inputData = $('#data');
+        this._inputValor = $('#valor');
+        this._inputQuantidade = $('#quantidade');
 
     }
     adiciona(event){
@@ -19,18 +19,28 @@ class NegociacaoController {
         //     this._inputQuantidade,
         //     this._inputValor,
         // ); other way is:
-
-        let negociacao = new Negociacao(
-            new Date(
-                this.inputData
-                .split('-')
-                .map(function(item, i){
-                    return (i == 1) ? item - 1 : item; 
-                })
-            ), 
-            this._inputQuantidade,
-            this._inputValor
+        let dataAjustada = new Date(...this.data.split('-')//[SPREAD OPERATOR] desestruturando um array para os parametro que uma função aceita, no exemplo: new Date(YYYY, MM, DD)
+            /* mas no exemplo do date o mês ele tem o padrão 0 - janeiro, 1 - fevereiro, etc.. 
+            então precisa diminuir de um o mês somente */    
+            .map((item, i) => (i == 1)?item-1 : item)  // em uma arrow function se eu só tenho uma instrução não preciso de {} e 'return'
         );
+        let negociacao = new Negociacao(
+            dataAjustada, 
+            this.quantidade,
+            this.valor
+        );
+
+        console.log(negociacao);
+    }
+
+    get data(){
+        return this._inputData.value;
+    }
+    get quantidade(){
+        return this._inputQuantidade.value;
+    }
+    get valor(){
+        return this._inputValor.value;
     }
 
 }
