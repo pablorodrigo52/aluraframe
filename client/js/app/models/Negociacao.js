5e4cd42fc589da567cea36a1caf260096abf372c
Pablo Rodrigo <pablo.santos@softgreen.com.br>
2020-05-31 14:29:28 -0300

    Creating 'private' attributes on classes js (javascript at this point do not have support to private things) and get methods

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,7 +1,26 @@
 class Negociacao{
+
     constructor(data, quantidade, valor){
-        this.data = data;
-        this.quantidade = quantidade;
-        this.valor = valor;
+        /* Existe uma convenção entre os desenvolvedores que atributos com underline não podem ser 
+        acessados ou modificados, mas isso não é uma regra, é só uma convenção porque atualmente
+        javascript não tem modificadores que bloqueiem o acesso ou escrita
+        */
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+    };
+    
+    getVolume(){
+        return this._quantidade * this._valor;
+    }
+
+    getData(){
+        return this._data;
+    }
+    getQuantidade(){
+        return this._quantidade;
+    }
+    getValor(){
+        return this._valor;
     }
 }
