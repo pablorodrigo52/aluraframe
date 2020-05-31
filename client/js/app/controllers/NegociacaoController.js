6a057d8806e3137797952c0707afaacfaf2a53d9
Pablo Rodrigo <pablo.santos@softgreen.com.br>
2020-05-31 16:04:47 -0300

    Work with map function

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,7 +1,36 @@
+/*
+Um controller é capaz de capturar as informações do DOM
+*/
+
 class NegociacaoController {
 
+    constructor (){
+        let $ = document.querySelector.bind(document); // cria um alias para a função document.querySelector('');
+        /* Simulando a sintaxe do jquery */    
+        this._inputDate = $('#date').value;
+        this._inputValor = $('#valor').value;
+        this._inputQuantidade = $('#quantidade').value;
+
+    }
     adiciona(event){
         event.preventDefault();
-        alert('cliqueeeei');
+        // let negociacao = new Negociacao(
+        //     new Date(this._inputDate.split('-')), // new Date(['YYYY', 'MM', 'DD']) == 'MM DD YYYY : 00h00m00s
+        //     this._inputQuantidade,
+        //     this._inputValor,
+        // ); other way is:
+
+        let negociacao = new Negociacao(
+            new Date(
+                this.inputData
+                .split('-')
+                .map(function(item, i){
+                    return (i == 1) ? item - 1 : item; 
+                })
+            ), 
+            this._inputQuantidade,
+            this._inputValor
+        );
     }
+
 }
