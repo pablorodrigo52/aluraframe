fa010b54f392e5a1f3c7a99577b2e5589becfd7d
Pablo Rodrigo <pablo.santos@softgreen.com.br>
2020-05-31 14:49:35 -0300

    A immutable Negociacao class

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -4,8 +4,13 @@ class Negociacao{
         /* Existe uma convenção entre os desenvolvedores que atributos com underline não podem ser 
         acessados ou modificados, mas isso não é uma regra, é só uma convenção porque atualmente
         javascript não tem modificadores que bloqueiem o acesso ou escrita
+
+         Quando minha variavel armazena objeto, como new Date() que é passado por parametro nesse caso
+         ele ainda pode ser alterado se nesse objeto existir algum método capaz disso. Isso acontece porque
+         Object.freeze é superficial, então para prevenir essa alteração faz-se uma cópia do objeto que é
+         passado
         */
-        this._data = data;
+        this._data = new Date(data.getTime()); 
         this._quantidade = quantidade;
         this._valor = valor;
         Object.freeze(this); // isso 'congela' um objeto em javascript o tornando imutável, mas ainda visivel
@@ -15,7 +20,7 @@ class Negociacao{
         return this._quantidade * this._valor;
     }
     get data(){
-        return this._data;
+        return new Date(this._data.getTime());
     }
     get quantidade(){
         return this._quantidade;
