57c311c5527149cbeb366d6fddbfdd1a7cce7ddf
Pablo Rodrigo <prlsantos@dcomp.ufs.br>
2020-08-01 20:06:03 -0300

    Adding function to import negotiations.. it's making a ajax call to server that runs on localhost:3000 port to tests and a JSON response is returned.

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -65,4 +65,30 @@ class NegociacaoController {
         this._inputData.focus();
     }
 
+
+    importarNegociacoes(){
+        let xhr = new XMLHttpRequest();
+
+        xhr.open('GET', 'negociacoes/semana');
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState == 4){
+                if (xhr.status == 200){
+                    
+                } else {
+                    throw new Error('Não foi possível obter as negociações do servidor');
+                }
+            }
+        };
+        xhr.send();
+
+        /**
+         * Estados possíveis de uma requisição ajax:
+         * 0: requisição ainda não iniciada
+         * 1: conexão com o servidor estabelecida
+         * 2: requisição recebida
+         * 3: processando requisição
+         * 4: requisição concluída e a resposta esta pronta
+         */
+    }
+
 }
